d88dd3500308a3029a62f796b9985c04116de7df
dsyurchenko96 <dsyurchenko96@gmail.com>
2026-08-25 05:44:57 +0300

    suppress and log httpx errors

--- src/s21_slot_bot/app/input_handler.py
+++ src/s21_slot_bot/app/input_handler.py
@@ -1,3 +1,4 @@
+import httpx
 import telegram
 from pydantic import ValidationError
 from telegram import Message, Update
@@ -107,6 +108,8 @@ class InputHandler:
                 await self._messenger.render_menu_error(context, error_description, logger)
             case Error():
                 await self._messenger.send(context, error_description)
+            case httpx.TransportError():
+                logger.exception("Error with connection, likely while trying to fetch updates, suppressing the error")
             case _:
                 await self._messenger.send(context, f"❌ неизвестная ошибка - {error_description}")
         if (job := context.job) and job.name and job.name != BOOKING_REFRESHER_JOB_NAME:
